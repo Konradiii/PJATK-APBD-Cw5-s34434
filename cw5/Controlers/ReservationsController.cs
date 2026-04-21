6be82a4027ed65d9aab752fb45d52389f47848a1
Konrad Karczewski <s34434@pjwstk.edu.pl>
2026-04-21 22:27:25 +0200

    Dodanie kontorlera ReservationsController

--- cw5/Controlers/ReservationsController.cs
+++ cw5/Controlers/ReservationsController.cs
@@ -1,3 +1,4 @@
+using cw5.DTO;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualBasic;
 
@@ -75,5 +76,111 @@ public class ReservationsController : ControllerBase
             Status = ReservationStatus.confirmed
         }
     ];
+    
+    
+    
+    [HttpGet]
+    public IActionResult GetAll(
+        [FromQuery] DateTime? date, 
+        [FromQuery] ReservationStatus? status,
+        [FromQuery] int? RoomId 
+        )
+    {
+        var result =
+            Reservations
+                .Where(r =>
+                    (date == null || r.Date == date) &&
+                    (status == null || r.Status == status) &&
+                    (RoomId == null || r.RoomId == RoomId)
+                )
+                .Select(r => new ReservationDto
+                {
+                    Id = r.Id,
+                    RoomId = r.RoomId,
+                    OrganizerName = r.OrganizerName,
+                    Topic = r.Topic,
+                    Date = r.Date,
+                    StartTime = r.StartTime,
+                    EndTime = r.EndTime,
+                    Status = r.Status
+
+                });
+        return Ok(result);
+    }
+
+    [HttpGet("{id:int}")]
+    public IActionResult GetById(int id)
+    {
+        var reservation =  Reservations.FirstOrDefault(r => r.Id == id);
+        
+        if (reservation == null)
+            return NotFound();
+        
+        return Ok(new ReservationDto
+        {
+            Id = reservation.Id,
+            RoomId = reservation.RoomId,
+            OrganizerName = reservation.OrganizerName,
+            Topic = reservation.Topic,
+            Date = reservation.Date,
+            StartTime = reservation.StartTime,
+            EndTime = reservation.EndTime,
+            Status = reservation.Status
+
+        });
+        
+    }
+
+    [HttpPost]
+    public IActionResult Create([FromBody] CreateReservationDto reservation)
+    {
+
+        var rez = new Reservation
+        {
+            Id = Reservations.Max(r => r.Id) + 1,
+            RoomId = reservation.RoomId,
+            OrganizerName = reservation.OrganizerName,
+            Topic = reservation.Topic,
+            Date = reservation.Date,
+            StartTime = reservation.StartTime,
+            EndTime = reservation.EndTime,
+            Status = reservation.Status
+        };
+        Reservations.Add(rez);
+        return CreatedAtAction(nameof(GetById), new { id = rez.Id }, rez);    }
+
+    [HttpPut("{id:int}")]
+    public IActionResult Update(int id, [FromBody] UpdateReservationDto reservation)
+    {
+        
+        var rez = Reservations.FirstOrDefault(r => r.Id == id);
+
+        if (rez == null)
+        {
+            return NotFound();
+        }
+
+        rez.OrganizerName = reservation.OrganizerName;
+        rez.Topic = reservation.Topic;
+        rez.Date = reservation.Date;
+        rez.StartTime = reservation.StartTime;
+        rez.EndTime = reservation.EndTime;
+        rez.Status = reservation.Status;
+
+        
+        return NoContent();
+    }
+    
+    [HttpDelete("{id:int}")]
+    public IActionResult Delete(int id)
+    {
+        var rez = Reservations.FirstOrDefault(r => r.Id == id);
+
+        if (rez == null)
+            return NotFound();
+
+        Reservations.Remove(rez);
+        return NoContent();
+    }
     
 }
